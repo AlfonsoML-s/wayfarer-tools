3beaa427175e699ba6dab2ce9097e4ed2b2b557c
Alfonso Martínez de Lizarrondo <amla70@gmail.com>
2019-11-03 17:14:58 +0100

    Remove unused S2 code

--- wayfarer-exporter.user.js
+++ wayfarer-exporter.user.js
@@ -253,7 +253,6 @@ function init() {
 */
 	const S2 = {};
 	const d2r = Math.PI / 180.0;
-	const r2d = 180.0 / Math.PI;
 
 	function LatLngToXYZ(latLng) {
 		const phi = latLng.lat * d2r;
@@ -261,13 +260,6 @@ function init() {
 		const cosphi = Math.cos(phi);
 
 		return [Math.cos(theta) * cosphi, Math.sin(theta) * cosphi, Math.sin(phi)];
-	}
-
-	function XYZToLatLng(xyz) {
-		const lat = Math.atan2(xyz[2], Math.sqrt(xyz[0] * xyz[0] + xyz[1] * xyz[1]));
-		const lng = Math.atan2(xyz[1], xyz[0]);
-
-		return {lat: lat * r2d, lng: lng * r2d};
 	}
 
 	function largestAbsComponent(xyz) {
@@ -315,33 +307,6 @@ function init() {
 		return [face, uv];
 	}
 
-	function FaceUVToXYZ(face, uv) {
-		const u = uv[0];
-		const v = uv[1];
-
-		switch (face) {
-			case 0: return [1, u, v];
-			case 1: return [-u, 1, v];
-			case 2: return [-u,-v, 1];
-			case 3: return [-1,-v,-u];
-			case 4: return [v,-1,-u];
-			case 5: return [v, u,-1];
-			default: throw {error: 'Invalid face'};
-		}
-	}
-
-	function STToUV(st) {
-		const singleSTtoUV = function (st) {
-			if (st >= 0.5) {
-				return (1 / 3.0) * (4 * st * st - 1);
-			} 
-			return (1 / 3.0) * (1 - (4 * (1 - st) * (1 - st)));
-			
-		};
-
-		return [singleSTtoUV(st[0]), singleSTtoUV(st[1])];
-	}
-
 	function UVToST(uv) {
 		const singleUVtoST = function (uv) {
 			if (uv >= 0) {
@@ -363,47 +328,6 @@ function init() {
 		};
 
 		return [singleSTtoIJ(st[0]), singleSTtoIJ(st[1])];
-	}
-
-	function IJToST(ij,order,offsets) {
-		const maxSize = 1 << order;
-
-		return [
-			(ij[0] + offsets[0]) / maxSize,
-			(ij[1] + offsets[1]) / maxSize
-		];
-	}
-
-	// hilbert space-filling curve
-	// based on http://blog.notdot.net/2009/11/Damn-Cool-Algorithms-Spatial-indexing-with-Quadtrees-and-Hilbert-Curves
-	// note: rather then calculating the final integer hilbert position, we just return the list of quads
-	// this ensures no precision issues whth large orders (S3 cell IDs use up to 30), and is more
-	// convenient for pulling out the individual bits as needed later
-	function pointToHilbertQuadList(x,y,order) {
-		const hilbertMap = {
-			'a': [[0,'d'], [1,'a'], [3,'b'], [2,'a']],
-			'b': [[2,'b'], [1,'b'], [3,'a'], [0,'c']],
-			'c': [[2,'c'], [3,'d'], [1,'c'], [0,'b']],
-			'd': [[0,'a'], [3,'c'], [1,'d'], [2,'d']]	
-		};
-
-		let currentSquare = 'a';
-		const positions = [];
-
-		for (let i = order - 1; i >= 0; i--) {
-
-			const mask = 1 << i;
-
-			const quad_x = x & mask ? 1 : 0;
-			const quad_y = y & mask ? 1 : 0;
-			const t = hilbertMap[currentSquare][quad_x * 2 + quad_y];
-
-			positions.push(t[0]);
-
-			currentSquare = t[1];
-		}
-
-		return positions;
 	}
 
 	// S2Cell class
@@ -430,70 +354,6 @@ function init() {
 
 	S2.S2Cell.prototype.toString = function () {
 		return 'F' + this.face + 'ij[' + this.ij[0] + ',' + this.ij[1] + ']@' + this.level;
-	};
-
-	S2.S2Cell.prototype.getCornerLatLngs = function () {
-		const offsets = [
-			[0.0, 0.0],
-			[0.0, 1.0],
-			[1.0, 1.0],
-			[1.0, 0.0]
-		];
-
-		return offsets.map(offset => {
-			const st = IJToST(this.ij, this.level, offset);
-			const uv = STToUV(st);
-			const xyz = FaceUVToXYZ(this.face, uv);
-
-			return XYZToLatLng(xyz);
-		});
-	};
-
-	S2.S2Cell.prototype.getFaceAndQuads = function () {
-		const quads = pointToHilbertQuadList(this.ij[0], this.ij[1], this.level);
-
-		return [this.face, quads];
-	};
-
-	S2.S2Cell.prototype.getNeighbors = function (deltas) {
-
-		const fromFaceIJWrap = function (face,ij,level) {
-			const maxSize = 1 << level;
-			if (ij[0] >= 0 && ij[1] >= 0 && ij[0] < maxSize && ij[1] < maxSize) {
-				// no wrapping out of bounds
-				return S2.S2Cell.FromFaceIJ(face,ij,level);
-			} 
-			// the new i,j are out of range.
-			// with the assumption that they're only a little past the borders we can just take the points as
-			// just beyond the cube face, project to XYZ, then re-create FaceUV from the XYZ vector
-
-			let st = IJToST(ij,level,[0.5, 0.5]);
-			let uv = STToUV(st);
-			let xyz = FaceUVToXYZ(face, uv);
-			const faceuv = XYZToFaceUV(xyz);
-			face = faceuv[0];
-			uv = faceuv[1];
-			st = UVToST(uv);
-			ij = STToIJ(st,level);
-			return S2.S2Cell.FromFaceIJ(face, ij, level);
-		};
-
-		const face = this.face;
-		const i = this.ij[0];
-		const j = this.ij[1];
-		const level = this.level;
-
-		if (!deltas) {
-			deltas = [
-				{a: -1, b: 0},
-				{a: 0, b: -1},
-				{a: 1, b: 0},
-				{a: 0, b: 1}
-			];
-		}
-		return deltas.map(function (values) {
-			return fromFaceIJWrap(face, [i + values.a, j + values.b], level);
-		});
 	};
 
 }
